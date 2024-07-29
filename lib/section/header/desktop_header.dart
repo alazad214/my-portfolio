d451862c2951f0f41b9f083cb895293b10131bd6
al azad <alazad214@gmail.com>
2024-07-29 22:53:00 +0600

    ui update

--- lib/section/header/desktop_header.dart
+++ lib/section/header/desktop_header.dart
@@ -11,7 +11,7 @@ class DesktopHeader extends StatelessWidget {
     return Container(
       alignment: Alignment.center,
       width: double.infinity,
-      padding: const EdgeInsets.symmetric(vertical: 15),
+      padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 10.0),
       margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(8.0),
@@ -19,18 +19,11 @@ class DesktopHeader extends StatelessWidget {
           boxShadow: const [
             BoxShadow(color: Colors.black, spreadRadius: 0.5, blurRadius: 5)
           ]),
-      child: const Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      child:  const Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text(
-            "Al Azad",
-            style: TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-                fontFamily: "TimeNew",
-                fontWeight: FontWeight.bold),
-          ),
+    Icon(Icons.code, color: Colors.white, size: 35.0,),
           CustomTabbar(),
           CustomResumeButton()
         ],
