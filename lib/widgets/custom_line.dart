39ef958d85583adbac903876482c6d5919fca827
al azad <alazad214@gmail.com>
2024-07-29 13:54:21 +0600

    custom line affect added

--- lib/widgets/custom_line.dart
+++ lib/widgets/custom_line.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hovering/hovering.dart';
 
 class CustomLine extends StatelessWidget {
   final String? text;
@@ -8,27 +9,53 @@ class CustomLine extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(
-      padding: padding??EdgeInsets.only(),
-      child: Row(
-        children: [
+    return HoverWidget(
+      hoverChild: Padding(
+        padding: padding??const EdgeInsets.only(),
+        child: Row(
+          children: [
 
-          Text(
-            text!,
-            style: const TextStyle(
-              color: Colors.white,
-              fontSize: 18,
+            Text(
+              text!,
+              style: const TextStyle(
+                color: Colors.blueAccent,
+                fontSize: 18,
+              ),
             ),
-          ),
-          const SizedBox(width: 10,),
-          const Expanded(
-            child: Divider(
-              color: Colors.white,
+            const SizedBox(width: 10,),
+            const Expanded(
+              child: Divider(
+                color: Colors.blueAccent,
+              ),
             ),
-          ),
-          const SizedBox(width: 10,),
-        ],
+            const SizedBox(width: 10,),
+          ],
+        ),
       ),
+      onHover: (event) {},
+      child: Padding(
+        padding: padding??const EdgeInsets.only(),
+        child: Row(
+          children: [
+
+            Text(
+              text!,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+              ),
+            ),
+            const SizedBox(width: 10,),
+            const Expanded(
+              child: Divider(
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(width: 10,),
+          ],
+        ),
+      )
     );
+
   }
 }
