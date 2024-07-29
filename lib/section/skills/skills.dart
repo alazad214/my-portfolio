b778e9a8e8a709ed42dfcd439a972b2cb54923e4
al azad <alazad214@gmail.com>
2024-07-29 13:42:54 +0600

    skills affect added

--- lib/section/skills/skills.dart
+++ lib/section/skills/skills.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hovering/hovering.dart';
 import 'package:portfolio/widgets/custom_line.dart';
 
 import '../../utils/platform.dart';
@@ -25,44 +26,85 @@ class Skills extends StatelessWidget {
             alignment: WrapAlignment.center,
             children: [
               for (int i = 0; i < skillItems.length; i++)
-                Container(
-                  width: width,
-                  alignment: Alignment.center,
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-                  margin:
-                      const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5.0),
-                      color: const Color(0xFF232227),
-                      boxShadow: const [
-                        BoxShadow(
-                            color: Colors.black,
-                            spreadRadius: 0.5,
-                            blurRadius: 5)
-                      ]),
-                  child: Column(
-                    children: [
-                      Image.asset(
-                        skillItems[i]["img"],
-                        width: 30,
+                HoverWidget(
+                    hoverChild: Container(
+                      width: width,
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 5),
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 5, vertical: 5),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5.0),
+                          color: const Color(0xFF232227),
+                          border: Border.all(color: Colors.blueAccent, width: 1.0),
+                          boxShadow: const [
+                            BoxShadow(
+                                color: Colors.black,
+                                spreadRadius: 0.5,
+                                blurRadius: 5)
+                          ]),
+                      child: Column(
+                        children: [
+                          Image.asset(
+                            skillItems[i]["img"],
+                            width: 30,
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            skillItems[i]["title"],
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 1,
+                            style: const TextStyle(
+                                color: Colors.white, fontSize: 14),
+                          ),
+                          const SizedBox(
+                            width: 20,
+                          ),
+                        ],
                       ),
-                      const SizedBox(
-                        height: 5,
+                    ),
+                    onHover: (event) {},
+                    child: Container(
+                      width: width,
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 5),
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 5, vertical: 5),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5.0),
+                          color: const Color(0xFF232227),
+                          boxShadow: const [
+                            BoxShadow(
+                                color: Colors.black,
+                                spreadRadius: 0.5,
+                                blurRadius: 5)
+                          ]),
+                      child: Column(
+                        children: [
+                          Image.asset(
+                            skillItems[i]["img"],
+                            width: 30,
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            skillItems[i]["title"],
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 1,
+                            style: const TextStyle(
+                                color: Colors.white, fontSize: 14),
+                          ),
+                          const SizedBox(
+                            width: 20,
+                          ),
+                        ],
                       ),
-                      Text(
-                        skillItems[i]["title"],
-                        overflow: TextOverflow.ellipsis,
-                        maxLines: 1,
-                        style:
-                            const TextStyle(color: Colors.white, fontSize: 14),
-                      ),
-                      const SizedBox(
-                        width: 20,
-                      ),
-                    ],
-                  ),
-                )
+                    )),
             ],
           ),
         )
